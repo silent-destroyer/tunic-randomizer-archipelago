e8275e40c1ad4ee7d79e86fa81e7df60e769ca42
Scipio Wright <scipiowright@gmail.com>
2024-01-28 19:59:10 -0500

    More work on entrance spell

--- src/Patches/PlayerCharacterPatches.cs
+++ src/Patches/PlayerCharacterPatches.cs
@@ -251,6 +251,8 @@ namespace TunicArchipelago {
             }
 
             FairyTargets.CreateFairyTargets();
+            FairyTargets.CreateEntranceTargets();
+            FairyTargets.FindFairyTargets();
 
             if (!SceneLoaderPatches.SpawnedGhosts) {
                 GhostHints.SpawnHintGhosts(SceneLoaderPatches.SceneName);
