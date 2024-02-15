c91befc0f0b1f94467c41fbe9526ffd72d480701
Scipio Wright <scipiowright@gmail.com>
2024-02-15 12:36:20 -0500

    Entrance Rando Refactor (#12)

--- src/Data/PortalCombo.cs
+++ src/Data/PortalCombo.cs
@@ -21,107 +21,29 @@ namespace TunicArchipelago {
             };
         }
 
-        // the idea is to do something like, we check if we can get any rewards from this portal combo based on what we have
-        // if we have the region a portal is in, we add that portal to the rewards list (if it meets the requirements)
-        public List<string> ComboRewards(Dictionary<string, int> inventory) {
-            List<string> rewardsList = new List<string>();
-
-            if (this.Portal1.Reachable(inventory)) {
-                rewardsList.Add(this.Portal1.SceneDestinationTag);
+        public bool CanReach(Dictionary<string, int> inventory) {
+            if (inventory.ContainsKey(this.Portal1.Region) || inventory.ContainsKey(this.Portal2.Region)) {
+                return true;
+            } else {
+                return false;
             }
-            if (this.Portal2.Reachable(inventory)) {
-                rewardsList.Add(this.Portal2.SceneDestinationTag);
-            }
+        }
 
-            // check if we can get to the other portal from the first portal
-            if (this.Portal1.Reachable(inventory)) {
-                List<string> entryItems = new List<string>();
-                if (this.Portal1.PrayerPortal) {
-                    entryItems.Add("12");
-                }
-                if (this.Portal1.EntryItems != null) {
-                    foreach (KeyValuePair<string, int> items in this.Portal1.EntryItems) {
-                        entryItems.Add(items.Key);
-                    }
-                }
-                int count = 0;
-                foreach (string item in entryItems) {
-                    // if it's the regular key, check that we have both keys already
-                    if (item == "Key") {
-                        if (inventory[item] == 2) {
-                            count++;
-                        }
-                    } else if (inventory.ContainsKey(item)) {
-                        count++;
-                    }
-                    //else
-                    //{
-                    //    Logger.LogInfo("inventory does not contain " + item);
-                    //}
-                }
-                // if we have all of the entry items, we get the portal
-                if (count >= entryItems.Count) {
-                    if (!inventory.ContainsKey(this.Portal2.SceneDestinationTag)) {
-                        inventory.Add(this.Portal2.SceneDestinationTag, 1);
-                    }
-                }
-                //else
-                //{
-                //    Logger.LogInfo("entry items did not succeed for " + this.Portal1.Name + " <-> " + this.Portal2.Name);
-                //}
-
+        public Dictionary<string, int> AddComboRegions(Dictionary<string, int> inventory) {
+            // if both regions are in here already, just skip it
+            if (inventory.ContainsKey(this.Portal1.Region) && inventory.ContainsKey(this.Portal2.Region)) {
+                return inventory;
             }
-
-            // and then we do the same for the second portal
-            if (this.Portal2.Reachable(inventory)) {
-                List<string> entryItems = new List<string>();
-                if (this.Portal2.PrayerPortal) {
-                    entryItems.Add("12");
+            // if you can reach this portal combo, add whichever region you don't have yet
+            if (this.CanReach(inventory)) {
+                if (!inventory.ContainsKey(this.Portal1.Region)) {
+                    inventory.Add(this.Portal1.Region, 1);
                 }
-                if (this.Portal2.EntryItems != null) {
-                    foreach (KeyValuePair<string, int> items in this.Portal2.EntryItems) {
-                        entryItems.Add(items.Key);
-                    }
+                if (!inventory.ContainsKey(this.Portal2.Region)) {
+                    inventory.Add(this.Portal2.Region, 1);
                 }
-                int count = 0;
-                foreach (string item in entryItems) {
-                    // if it's the regular key, check that we have both keys already
-                    if (item == "Key") {
-                        if (inventory[item] == 2) {
-                            count++;
-                        }
-                    } else if (inventory.ContainsKey(item)) {
-                        count++;
-                    }
-                    //else
-                    //{
-                    //    Logger.LogInfo("inventory does not contain " + item);
-                    //}
-                }
-                // if we have all of the entry items, we get the portal
-                if (count == entryItems.Count) {
-                    if (!inventory.ContainsKey(this.Portal1.SceneDestinationTag)) {
-                        inventory.Add(this.Portal1.SceneDestinationTag, 1);
-                    }
-                }
-                //else
-                //{
-                //    Logger.LogInfo("entry items did not succeed for " + this.Portal2.Name + " <-> " + this.Portal1.Name);
-                //}
             }
-            //}
-            // and now, we grab the regions or portals that we're given access to from having either portal and add them to the rewards list
-            if (inventory.ContainsKey(this.Portal1.SceneDestinationTag)) {
-                rewardsList.AddRange(this.Portal1.Rewards(inventory).Except(rewardsList));
-                if (this.Portal2.Scene == "Shop" && !inventory.ContainsKey("Shop")) { inventory.Add("Shop", 1); }
-            }
-
-            if (inventory.ContainsKey(this.Portal2.SceneDestinationTag)) {
-                rewardsList.AddRange(this.Portal2.Rewards(inventory).Except(rewardsList));
-                if (this.Portal2.Scene == "Shop" && !inventory.ContainsKey("Shop")) { inventory.Add("Shop", 1); }
-            }
-
-            return rewardsList;
+            return inventory;
         }
     }
 }
